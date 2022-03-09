3be48f04d4e6eb5fdb12f39513843cf10ceee256
Julius Walton <waltonjj@g.cofc.edu>
2022-03-09 12:49:06 -0500

    Add banner component

---
+++ src/api/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = 'e58e0db59857ad066933855512dd9f89';
+const YOUTUBE = 'AIzaSyAOYQuvVvvyKvtpZIcA88zgsVqa_c9Lc7g';
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTrailers: `https://youtube.googleapis.com/youtube/v3/search?key=${YOUTUBE}`,
+};
+export default requests;
--- src/components/Billboard/Billboard.jsx
+++ src/components/Billboard/Billboard.jsx
@@ -1,8 +1,76 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from '../../api/axios';
+import requests from '../../api/requests';
+import YouTube from 'react-youtube';
 import styles from './Billboard.module.css';
+
 const Billboard = () => {
-  return <div className={styles.billboard}>Billboard</div>;
+  const [movie, setMovie] = useState([]);
+  const [trailerURL, setTrailerURL] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+
+      let currentMovie = request.data.results[Math.floor(Math.random() * request.data.results.length - 1)];
+      while (!currentMovie.backdrop_path) {
+        currentMovie = request.data.results[Math.floor(Math.random() * request.data.results.length - 1)];
+      }
+
+      setMovie(currentMovie);
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + '...' : str;
+  }
+
+  const handleClick = movie => {
+    axios
+      .get(requests.fetchTrailers, {
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/json' },
+        params: {
+          q: `${movie.name} trailer`,
+          maxResults: 1,
+        },
+      })
+      .then(res => {
+        setTrailerURL(res.data.items[0].id.videoId);
+      });
+  };
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  return (
+    <div className={styles.container}>
+      <header
+        className={styles.header}
+        onClick={() => setTrailerURL('')}
+        style={{ backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")` }}
+      >
+        <div className={styles.contents}>
+          <h1 className={styles.title}>{movie?.title || movie?.name || movie?.original_name}</h1>
+          <div className={styles.buttonGroup}>
+            <button onClick={() => handleClick(movie)} className={styles.button}>
+              Play
+            </button>
+            <button className={styles.button}>My List</button>
+          </div>
+          <h1 className={styles.description}>{truncate(movie?.overview, 150)}</h1>
+        </div>
+        <div className={styles.fadeBottom} />
+      </header>
+      {trailerURL && <YouTube videoId={trailerURL} opts={opts} />}
+    </div>
+  );
 };
 
 export default Billboard;
